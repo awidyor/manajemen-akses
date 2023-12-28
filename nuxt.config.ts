b9d68b3f1c8380dd30518770b5e50fc7c96bdf68
Ikram Maulana <ikram075@ummi.ac.id>
2023-12-29 06:39:19 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  build: {
+    transpile: [
+      'trpc-nuxt'
+    ]
+  },
+  typescript: {
+    shim: false
+  }
+})
