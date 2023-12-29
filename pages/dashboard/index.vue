ad6825b2a69f9fb7292851a6c1410b4225b4decd
Ikram Maulana <ikram075@ummi.ac.id>
2023-12-29 07:38:52 +0700

    chore(auth): initialize kinde auth

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello Dashboard</h1>
+    <NuxtLink to="/api/logout" external>
+      Sign out
+    </NuxtLink>
+    {{ $auth.user }}
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['auth-logged-in']
+})
+</script>
