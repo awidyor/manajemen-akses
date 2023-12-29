b9f2c8c6cceae948c920f8d995bb499c8acbeca8
Ikram Maulana <ikram075@ummi.ac.id>
2023-12-29 09:40:51 +0700

    chore(layout): implement dashboard layout
    - add dashboard layout
    - add heroIcons
    - add logo

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+      <TopbarDashboard :name="$auth.user.name" :email="$auth.user.email" />
+
+      <SidebarDashboard />
+
+      <main class="p-4 md:ml-64 h-auto pt-20">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite()
+})
+</script>
