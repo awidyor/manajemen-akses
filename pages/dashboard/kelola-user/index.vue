fbe74a530b20cb217025c299df947f4ecbaef550
Deri-Kurniawan <deri.netuchi@gmail.com>
2023-12-30 15:45:10 +0700

    feat(crud): add CRUD user
    - add CRUD jabatan
    - add schema jabatan
    - add prisma ctx
    - add TRPC route
    - remove kelola akses

--- pages/dashboard/kelola-user/index.vue
+++ pages/dashboard/kelola-user/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
     <NuxtLayout>
-      <h1 class="scroll-m-20 text-2xl font-semibold tracking-tight mb-4">
+      <h1 class="mb-4 text-2xl font-semibold tracking-tight scroll-m-20">
         Kelola User
       </h1>
+
+      <DashboardNewUser />
+
+      <DashboardTableUser :data="data" />
     </NuxtLayout>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup type="ts">
+import { initFlowbite } from 'flowbite'
+const { $client } = useNuxtApp()
+
+const { data } = await $client.user.getAll.useQuery()
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite()
+})
+
 useHead({
   title: 'Dashboard | Kelola User'
 })
--- server/trpc/routers/index.ts
+++ server/trpc/routers/index.ts
@@ -1,8 +1,10 @@
 import { router } from '../trpc'
 import { jabatanRouter } from './jabatan'
+import { userRouter } from './user'
 
 export const appRouter = router({
-  jabatan: jabatanRouter
+  jabatan: jabatanRouter,
+  user: userRouter
 })
 
 // export type definition of API
