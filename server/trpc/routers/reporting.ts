735587ca06203fa86cee713a8bbb3ede81a480f8
Ikram Maulana <ikram075@ummi.ac.id>
2024-01-04 13:50:05 +0700

    chore(api): add create reporting tRPC API

--- server/trpc/routers/reporting.ts
+++ server/trpc/routers/reporting.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod'
 import { publicProcedure, router } from '../trpc'
 
 export const reportingRouter = router({
@@ -12,5 +13,44 @@ export const reportingRouter = router({
           }
         }
       })
+    }),
+
+  create: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        tempatMasuk: z.string(),
+        metodeMasuk: z.string(),
+        metodeKeluar: z.string().optional(),
+        waktuMasuk: z.string(),
+        waktuKeluar: z.string().optional()
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      try {
+        const akses = await ctx.prisma.akses.create({
+          data: {
+            user: {
+              connect: {
+                id: input.userId
+              }
+            },
+            tempatMasuk: input.tempatMasuk,
+            metodeMasuk: input.metodeMasuk,
+            metodeKeluar: input.metodeKeluar,
+            waktuMasuk: input.waktuMasuk,
+            waktuKeluar: input.waktuKeluar
+          }
+        })
+
+        return akses
+      } catch (error) {
+        console.log(error)
+      }
+    }),
+
+  hello: publicProcedure
+    .query(async ({ ctx }) => {
+      return 'Hello World'
     })
 })
